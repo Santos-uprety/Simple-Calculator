432ba29933029f7b69ada86a44fefc4771b6f8fc
Santos-uprety uprety <upretysantos6@gmail.com>
2023-08-06 10:26:14 +0000

    Defining function to calculate based on button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 const display = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button")
+const specialCharacter = ["%", "/", "*", "+", "-", "="];
+let output ="";
 
-
+/** 
+ * Adding function to calculate based on button clicked.
+*/
 const calculate = (btnValue) => {
-    console.log(btnValue);
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+       output = ""; 
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialCharacter.includes(btnValue)) return;
+        output += btnValue;
+    }
+
+    display.value = output;
 };
 
 /**
