f3af2b3ce9775f29bff329520e87a4234425a802
Santos-uprety uprety <upretysantos6@gmail.com>
2023-08-06 09:53:37 +0000

    Add event listener to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 const display = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button")
-console.log(display, buttons);
+
+
+const calculate = (btnValue) => {
+    console.log(btnValue);
+};
+
+/**
+ * Adding event listener to buttons and calling click
+ */
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
